312b5355ec1f25ff4665457ad476205736c5bb8c
metelci <metelci@gmail.com>
2025-09-30 23:41:38 +0300

    update build.gradle.kts for improved compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.mtlc.studyplan"
         minSdk = 30
         targetSdk = 35
-        versionCode = 64
-        versionName = "2.9.20"
+        versionCode = 65
+        versionName = "2.9.28"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
